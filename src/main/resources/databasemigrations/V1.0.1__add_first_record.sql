6d38393cafe8a91cb7d3a205871f06035bb2579e
Joost van der Griendt <joostvdg@gmail.com>
2020-02-02 12:47:04 +0100

    add initial record
    Signed-off-by: Joost van der Griendt <joostvdg@gmail.com>

---
+++ src/main/resources/databasemigrations/V1.0.1__add_first_record.sql
@@ -0,0 +1,3 @@
+INSERT INTO GenerationRequests
+    (request_id, generation_count, duration, parameters, map_type, game_type, timestamp_request, host, user_agent)
+    VALUES ('ABASKDJASHJKD9083984', 1000, 100, ARRAY['max300=20'],'Large', 'Normal', current_timestamp, 'localhost', 'me');
